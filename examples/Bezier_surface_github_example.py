c50dfdb256eb03a5aa8377eea8d190d71d3b652b
reeceotto <71361841+reeceotto@users.noreply.github.com>
2021-01-06 15:19:08 +1000

    Update Bezier_surface_github_example.py

--- examples/Bezier_surface_github_example.py
+++ examples/Bezier_surface_github_example.py
@@ -8,13 +8,14 @@ import visualisation as visual
 from math import sqrt
 
 # Bezier surface example - https://gist.github.com/orbingol/05f0f6930331b7c3007644c31ae6e4bc
-surface = gc.NURBSSurface()
+# currently executed with a NURBS surface object, will update if Bezier functionality is added
+surface = gc.NURBS.Surface()
 
 # define control points
-surface.controlPoints = [[[0, 0, 0, 1], [0, 4, 0, 1], [0, 8, -3, 1]],
-                         [[2, 0, 6, 1], [2, 4, 0, 1], [2, 8, 0, 1]],
-                         [[4, 0, 0, 1], [4, 4, 0, 1], [4, 8, 3, 1]],
-                         [[6, 0, 0, 1], [6, 4, -3, 1], [6, 8, 0, 1]]]
+surface.controlPoints = [[[0, 0, 0], [0, 4, 0], [0, 8, -3]],
+                         [[2, 0, 6], [2, 4, 0], [2, 8, 0]],
+                         [[4, 0, 0], [4, 4, 0], [4, 8, 3]],
+                         [[6, 0, 0], [6, 4, -3], [6, 8, 0]]]
 
 # define control point weightings
 surface.weights = [[1, 1, 1],
@@ -31,5 +32,5 @@ surface.knotVector1 = gf.KnotVector(len(surface.controlPoints), surface.degree1)
 surface.knotVector2 = gf.KnotVector(len(surface.controlPoints[0]), surface.degree2)
 
 # create surface plot
-visual.SurfacePlot(surface, N1=50, N2=50)
+visual.SurfacePlot(surface)
 
