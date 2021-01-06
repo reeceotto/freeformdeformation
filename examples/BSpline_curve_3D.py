a281d4ddf053b02d5f2f8d5d995371a34bfe85e5
reeceotto <71361841+reeceotto@users.noreply.github.com>
2021-01-06 15:19:45 +1000

    Update BSpline_curve_3D.py

--- examples/BSpline_curve_3D.py
+++ examples/BSpline_curve_3D.py
@@ -6,7 +6,7 @@ import geom_classes as gc
 import geom_functions as gf
 import visualisation as visual
 
-curve = gc.BSplineCurve()
+curve = gc.BSpline.Curve()
 
 # define control points
 curve.controlPoints = [[1, -3, 2], [1, 2, 1], [2, 1, 0], [3, 3, -2]]
@@ -19,7 +19,7 @@ nControlPoints = len(curve.controlPoints)
 curve.knotVector = gf.KnotVector(nControlPoints, curve.degree)
 
 # create 3D curve plot
-visual.CurvePlot(curve, showControlPoints=True, showKnots=True, dimension='3D', start=0.1, stop=0.9, N=100)
+visual.CurvePlot(curve, showControlPoints=True, showKnots=True, plotDimension='3D', start=0.1, stop=0.9)
 
 # uncomment the following to produce 3D curve plot without knots, control points or control polygon
 #visual.CurvePlot(curve, dimension='3D', showControlPoints=False, showKnots=False, showControlPolygon=False, N=200)
