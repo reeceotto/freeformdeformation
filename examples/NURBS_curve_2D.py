710351afee9969fb6389c50c6c0973059c0d41dd
reeceotto <71361841+reeceotto@users.noreply.github.com>
2021-01-06 15:21:02 +1000

    Update NURBS_curve_2D.py

--- examples/NURBS_curve_2D.py
+++ examples/NURBS_curve_2D.py
@@ -6,7 +6,7 @@ import geom_classes as gc
 import geom_functions as gf
 import visualisation as visual
 
-curve = gc.NURBSCurve()
+curve = gc.NURBS.Curve()
 
 # define control points
 curve.controlPoints = [[1, 0, 0], [1, 2, 0], [2, 1, 0], [3, 3, 0]]
@@ -22,7 +22,7 @@ nControlPoints = len(curve.controlPoints)
 curve.knotVector = gf.KnotVector(nControlPoints, curve.degree)
 
 # create 2D curve plot
-visual.CurvePlot(curve, dimension='2D', start=0.0, stop=1.8)
+visual.CurvePlot(curve, plotDimension='2D', start=0.0, stop=1.8)
 
 # uncomment the following to produce 2D curve plot without knots, control points or control polygon
 #visual.CurvePlot(curve, dimension='2D', showControlPoints=False, showControlPolygon=False, showKnots=False, N=200)
